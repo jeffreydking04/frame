baf9511fd69e1edb3f53ff93f464697ac62b04cb
Jeffrey King <jdking33@aol.com>
2017-02-01 09:48:48 -0800

    All fixed

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,11 +1,16 @@
 require "frame/version"
+require "frame/utility"
+require "frame/router"
 require "frame/dependencies"
 require 'frame/controller'
 
 module Frame
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["Hello World"]]
+      stuff = controller_and_action(env)
+      thing = stuff[0].new(env)
+      text = thing.method(stuff[1].to_sym).call
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 end
--- lib/frame/router.rb
+++ lib/frame/router.rb
@@ -3,7 +3,7 @@ module Frame
     def controller_and_action(env)
       _, controller, action, _ = env['PATH_INFO'].split('/', 4)
       controller = controller.capitalize
-      controller = '#{controller}Controller'
+      controller = "#{controller}Controller"
 
       [Object.const_get(controller), action]
     end
